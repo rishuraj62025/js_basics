701764285fadf74b0be276fdd2b1f8075c0d2632
RISHU RAJ <130795176+rishuraj62025@users.noreply.github.com>
2024-10-23 16:45:27 +0000

    config

---
+++ 01_basics/datatypes3.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3 ) -> we are using nodejs not browser
+
+console.log(4+5)
+console.log("Rishu")
+
+// code readability should be high
+
+let name="Rishu"     // string
+let age=22           // number
+let isLoggedIn=true  // boolean
+
+// bigint
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "5");
+console.log(typeof 10);
