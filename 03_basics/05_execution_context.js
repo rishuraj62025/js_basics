4626fdc30d4e3947ca5cf2cef79f51795940e00a
RISHU RAJ <130795176+rishuraj62025@users.noreply.github.com>
2024-11-23 10:39:09 +0000

    execution context

--- 03_basics/05_execution_context.js
+++ 03_basics/05_execution_context.js
@@ -4,7 +4,7 @@
 // {} -> Global Execution Context   , jb bhi program execute hona shuru hoga ,ek Global Execution Context banega aur wo this variable mein store hoga
 // Global Execution Context ko ek "this" variable mein refer kr diya jata hai
 
-// ab browswer ka Global Execution Context alag hota hai aur node js ka Global Execution Context alag hota hai
+// ab browser ka Global Execution Context alag hota hai aur node js ka Global Execution Context alag hota hai
 // to browser ke andar this ki value window object aata hai
 // javascript single threaded hai
 
@@ -21,4 +21,54 @@
 // During execution phase, agar koi bhi function hota hai to uska ek alag execution context bnta hai
 // jisme ek new variable environment bnta hai aur ek execution thread bnta hai
 // jitna baar function execute hota hai utna baar uske liye new executional context bnta hai
-// Ab jitni baar function execute hoga utni baar, memory phase and execution phase chalega
+// Ab jitni baar function execute hoga, utni baar memory phase and execution phase chalega
+
+
+
+// ex:
+// let val1 = 5
+// let val2 = 8
+// function addNum(num1, num2)
+// {
+//     let total = num1 + num2
+//     return total
+// }
+
+// let result1 = addNum(val1, val2)
+// let result2 = addNum(10, 2)
+
+
+// step 1 : global execution context 
+// step 2 : memory creation phase 
+// val1 <- undefined
+// val2 <- undefined
+// addNum <- definition
+// result 1 <- undefined
+// result 2 <- undefined
+// step 3 : execution phase
+// val1 <- 5
+// val2 <- 8
+// addNum -> new variable environment  +  execution thread 
+
+// ab jitni baar function call hoga utni baar execution context bnega
+// ab yahan pe addNum function hai to iska execution context bnega
+// jisme phir se memory phase and execution context bnega
+
+// ab memory phase
+// val1 <- undefined
+// val2 <- undefined
+// total <- undefined
+
+// execution context
+// num1 <- 5
+// num2 <- 8
+// total = 8 + 5 = 13
+// result1 = 13
+
+// ab iss execution context ka work finish ho gya to ab ye delete bhi ho jaega
+
+// ab addNum function phir se call hua hai to ab phir se execution context create hoga
+// isme bhi same step hoga memory phase , execution context
+
+// call stack : jitne bhi functions hain code mein wo sb execute hote hain lifo manner mein
+
