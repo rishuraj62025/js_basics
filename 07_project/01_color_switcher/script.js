fae7e7b88c4d5bc6195a5ff2b3e86f8930929c94
RISHU RAJ <130795176+rishuraj62025@users.noreply.github.com>
2025-02-17 10:42:51 +0000

    project

--- 07_project/01_color_switcher/script.js
+++ 07_project/01_color_switcher/script.js
@@ -3,7 +3,7 @@ const body = document.querySelector('body');
 
 buttons.forEach(function(button){
     console.log(button);
-  button.addEventListener('click',function (e) {
+    button.addEventListener('click',function (e) {
     console.log(e);
     console.log(e.target);
     if(e.target.id === 'grey')
