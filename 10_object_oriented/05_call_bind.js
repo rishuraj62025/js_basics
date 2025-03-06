c7687877f6fbaef09658f27f30bb91d3a17491ad
RISHU RAJ <130795176+rishuraj62025@users.noreply.github.com>
2025-03-06 17:38:51 +0000

    call

--- 10_object_oriented/05_call_bind.js
+++ 10_object_oriented/05_call_bind.js
@@ -6,7 +6,9 @@ function SetUserName(username)
 
 function createUser(username, email, password)
 {
-    // yahan pe this as a reference bhejna prta hai
+    // yahan pe this as a reference bhejna prta hai, yahan hm chahte hain hm apna this bhej ke store karaye value,
+    // call function reference hold krke rkhta hai ki kahan se call hua hai
+    // call ka use krte hain jb ek function se dusre function ko call kr rhe
     SetUserName.call(this,username)
 
     this.email = email
@@ -15,5 +17,5 @@ function createUser(username, email, password)
 
 const chai = new createUser("chai", "abc@gmail.com", "123")
 console.log(chai);
-// o/p:  createUser  username: 'chai', email: 'abc@gmail.com', password: '123'}
+// o/p:  createUser { username: 'chai', email: 'abc@gmail.com', password: '123'}
   
